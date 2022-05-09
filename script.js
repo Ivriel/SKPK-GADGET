076afd0b1715108f1b27176eaf5199f311a3845d
Ivriel <105188925+Ivriel@users.noreply.github.com>
2022-05-09 11:57:03 +0700

    FIRST UPLOAD

---
+++ script.js
@@ -0,0 +1,93 @@
+let gadget =  [];
+
+let object = {
+    foto:"https://cdn1.katadata.co.id/media/images/thumb/2021/10/28/iPhone_13_Pro-2021_10_28-14_49_09_14b4d070187b2b687fe2affe100d8934_620x413_thumb.png",
+    harga:22000000,
+    kode:"BRG001",
+    nama:"iPhone 13 pro max",
+    rating:"5.0",
+    stock:10
+}
+gadget.push(object);
+let object2 = {
+    foto:"https://i.ytimg.com/vi/eYxHU5FSpdw/maxresdefault.jpg",
+    harga:Number(5000000),
+    kode:"BRG002",
+    nama:"Samsung A7 lite",
+    rating:"5.0",
+    stock:20
+}
+gadget.push(object2);
+let object3 = {
+    foto:"http://p-id.ipricegroup.com/uploaded_981215404e48e1628546376947a715f4.jpg",
+    harga:7000000,
+    kode:"BRG003",
+    nama:"Oppo Reno 9",
+    rating:"5.0",
+    stock:100
+}
+gadget.push(object3);
+let object4 = {
+    foto:"https://i01.appmifile.com/webfile/globalimg/id/cms/10B4246A-5AB3-13F7-B7F7-082776A7B5E7.jpg",
+    harga:5000000,
+    kode:"BRG004",
+    nama:"Xiaomi Redmi Note 11",
+    rating:"5.0",
+    stock:1000
+}
+gadget.push(object4);
+let object5 = {
+    foto:"https://asia-exstatic-vivofs.vivo.com/PSee2l50xoirPK7y/1633932774871/4d84ff85c24479acbfa7ebc413a8a9cc.png",
+    harga:6000000,
+    kode:"BRG005",
+    nama:"Vivo",
+    rating:"5.0",
+    stock:50
+}
+gadget.push(object5);
+let object6 = {
+    foto:"https://asset.kompas.com/crops/PA6U95yePYAoPmMChwjqABSml4I=/199x175:1577x1094/750x500/data/photo/2019/05/09/2193395800.jpg",
+    harga:3000000,
+    kode:"BRG006",
+    nama:"Realme",
+    rating:"5.0",
+    stock:50
+}
+gadget.push(object6);
+let object7 = {
+    foto:"https://www.notebookcheck.net/uploads/tx_nbc2/HuaweiY9Prime2019__1_.JPG",
+    harga:4000000,
+    kode:"BRG007",
+    nama:"Huawei y9",
+    rating:"5.0",
+    stock:50
+}
+gadget.push(object7);
+let object8 = {
+    foto:"https://images.tokopedia.net/img/cache/700/product-1/2019/5/19/66139657/66139657_e80748a4-e5ef-4a11-ae99-1790e3d21070_800_800",
+    harga:5000000,
+    kode:"BRG008",
+    nama:"Huawei y7",
+    rating:"3.0",
+    stock:20
+}
+gadget.push(object8);
+let object9 = {
+    foto:"https://fdn2.gsmarena.com/vv/bigpic/huawei-p40-pro.jpg",
+    harga:3000000,
+    kode:"BRG009",
+    nama:"Huawei p40 pro",
+    rating:"4.0",
+    stock:10
+}
+gadget.push(object9);
+let object10 = {
+    foto:"https://cdn.eraspace.com/pub/media/catalog/product/r/e/realme-6-comet-white-1_2.jpg",
+    harga:7000000,
+    kode:"BRG010",
+    nama:"Realme 6",
+    rating:"5.0",
+    stock:30
+}
+gadget.push(object10);
+console.log(gadget);
